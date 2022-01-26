e5ad43516619c72a0b8af346c8f31b5633971006
SethWack <sethwack@gmail.com>
2022-01-26 16:13:39 +0000

    added vendor

---
+++ huertaLimones/vendor/laravel/fortify/src/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Laravel\Fortify\Http\Controllers;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\Models\User;
+use App\Models\User_cars;
+use App\Models\Carritos;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Fortify\Contracts\RegisterResponse;
+use Laravel\Fortify\Contracts\RegisterViewResponse;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * The guard implementation.
+     *
+     * @var \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected $guard;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\StatefulGuard  $guard
+     * @return void
+     */
+    public function __construct(StatefulGuard $guard)
+    {
+        $this->guard = $guard;
+    }
+
+    /**
+     * Show the registration view.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Laravel\Fortify\Contracts\RegisterViewResponse
+     */
+    public function create(Request $request): RegisterViewResponse
+    {
+        return app(RegisterViewResponse::class);
+    }
+
+    /**
+     * Create a new registered user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Laravel\Fortify\Contracts\CreatesNewUsers  $creator
+     * @return \Laravel\Fortify\Contracts\RegisterResponse
+     */
+    public function store(Request $request,
+                          CreatesNewUsers $creator): RegisterResponse
+    {
+        event(new Registered($user = $creator->create($request->all())));
+        $car = Carritos::orderByDesc('id')->first();
+        $carId = $car['id'];
+        $users = User::latest()->first();
+        $user_id = $users['id'];
+        User_cars::create([
+            'USER_ID' => $user_id,
+            'CAR_ID' => $carId,
+        ]);
+        $this->guard->login($user);
+
+        return app(RegisterResponse::class);
+    }
+}
